7b76a7be8d111d3526a607483d19afdec005f3e9
James olujae <96151013+Jamesolujae123@users.noreply.github.com>
2024-07-04 18:42:41 -0700

    Create config.production.js

---
+++ libs/config.production.js
@@ -0,0 +1,20 @@
+const logger = require("./logger.js");
+require("dotenv").config();
+
+module.exports = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  params: {
+    dialect: "sqlite",
+    storage: "recipes_db.sqlite",
+    logging: (sql) => {
+      logger.info(`[${new Date()}] ${sql}`);
+    },
+    define: {
+      underscored: true,
+    },
+  },
+  jwtSecret: process.env.JWT_SECRET,
+  jwtSession: { session: false },
+};
